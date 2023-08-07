c93b4bf71ab7b1be687db2fef0912162df61bea4
John Freeman <jlfreeman@fastmail.com>
2023-08-07 19:05:14 -0400

    Configure client-side validation

--- src/templates/rootTemplate.ts
+++ src/templates/rootTemplate.ts
@@ -8,10 +8,13 @@ const rootTemplate = html`<!doctype html>
 			<meta name="description" content="Website Parser that outputs the relevant content from a given website" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<script src="/public/htmx.min.js"></script>
-			<script src="/public/_hyperscript.min.js"></script>
+			<script src="/public/loading-states.js"></script>
 			<link rel="stylesheet" href="/public/app.css" />
 		</head>
-		<body class="flex min-h-screen flex-col items-center justify-center bg-gray-300 font-body">
+		<body
+			hx-ext="loading-states"
+			class="flex min-h-screen flex-col items-center justify-center bg-gray-300 font-body"
+		>
 			<div class="m-2 flex flex-col rounded bg-white p-4">
 				<h1 class="p-1 text-center font-title text-4xl font-bold text-green-700 xs:text-5xl">Website Parser</h1>
 				<form
@@ -28,15 +31,35 @@ const rootTemplate = html`<!doctype html>
 						name="url"
 						placeholder="https://www.example.com"
 						required
-						class="peer mb-2 w-full rounded border-2 invalid:border-red-500 invalid:text-red-500"
+						class="peer mb-2 w-full rounded border-2 [:invalid&:not(:placeholder-shown)]:border-red-500 [:invalid&:not(:placeholder-shown)]:text-red-500"
 					/>
-					<p class="mb-2 hidden text-red-500 peer-invalid:inline">Provide a valid URL</p>
+					<p class="mb-2 hidden text-red-500 peer-[:invalid&:not(:placeholder-shown)]:block">
+						Provide a valid URL
+					</p>
 					<button
 						id="submit"
-						class="flex max-w-max rounded bg-blue-500 p-2 text-white hover:bg-blue-600 active:bg-blue-700"
+						data-loading-disable
+						class="flex max-w-max rounded bg-blue-500 p-2 text-white enabled:hover:bg-blue-600 enabled:focus:bg-blue-600 enabled:active:bg-blue-700 disabled:opacity-50"
 					>
 						Submit
 					</button>
+					<script>
+						htmx.onLoad(function (evt) {
+							const url = document.getElementById('url');
+							if (url) {
+								url.focus();
+								const button = document.getElementById('submit');
+								if (button && url.checkValidity()) button.disabled = false;
+								else if (button) button.disabled = true;
+							}
+						});
+						htmx.on('input', function (evt) {
+							const url = document.getElementById('url');
+							const button = document.getElementById('submit');
+							if (url && button && url.checkValidity()) button.disabled = false;
+							else if (button) button.disabled = true;
+						});
+					</script>
 				</form>
 			</div>
 		</body>
